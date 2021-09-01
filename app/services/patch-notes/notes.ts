520d49df182f56a832b48572325387e0fa4ba761
Akihiko Koizuka <koizuka@dwango.co.jp>
2021-09-01 14:02:18 +0900

    v1.1.20210901-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,10 +1,16 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20210727-unstable.1',
-  title: '1.1.20210727-unstable.1',
+  version: '1.1.20210901-unstable.1',
+  title: '1.1.20210901-unstable.1',
   notes: [
-    '追加: 新NVENC, 音声48kHz, 配信IPアドレス選択, リプレイ録画機能など (#505) by koizuka',
-    'コメント読み上げ辞書更新(初見追加) (#503) by koizuka'
-  ]
+    '修正: コメント読み上「生主」「放送主」 (#524) by koizuka',
+    '修正: カスタムURLスキーム "n-air-app" を登録して起動出来るようにする (#86) by koizuka',
+    '修正: コメント読み上げ「古参」 (#510) by koizuka',
+    '修正: ニコ生の番組を終了したら即座に配信を終了する (#516) by koizuka',
+    '修正: リンクを中ボタン(ホイール)クリックしても操作できないウィンドウが開かないようにする (#517) by koizuka',
+    '修正: 番組予約時刻30分以上前から開いていた場合、開始時刻に番組開始に切り替わらなかった (#521) by koizuka',
+    '修正:シーンの名前変更で (1) が付加されないようにする (#518) by koizuka',
+    '修正:設定/出力(詳細)/録画のエンコーダーの選択肢の日本語訳を追加 (#508) by koizuka',
+  ],
 };
