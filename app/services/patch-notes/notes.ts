32215f6bd54efa8cc8496bc6881c7a54ed85723b
Akihiko Koizuka <koizuka@dwango.co.jp>
2022-03-02 13:00:15 +0900

    v1.1.20220302-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,7 +1,10 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20211021-unstable.1',
-  title: '1.1.20211021-unstable.1',
-  notes: ['追加: 募集した単語リストを読み上げ辞書に追加 (#528) by koizuka'],
+  version: '1.1.20220302-unstable.1',
+  title: '1.1.20220302-unstable.1',
+  notes: [
+    '追加:コンパクトモードを追加する (#540) by koizuka',
+    '変更:ナビゲーションバーを上部から左に移動し、デザインを変更',
+  ],
 };
