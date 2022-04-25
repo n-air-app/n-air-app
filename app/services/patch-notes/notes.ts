5ba724cf53c58c8ea2494071e8537a05ef86dc99
Akihiko Koizuka <koizuka@dwango.co.jp>
2022-04-25 13:33:58 +0900

    v1.1.20220425-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,10 +1,14 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20220302-unstable.1',
-  title: '1.1.20220302-unstable.1',
+  version: '1.1.20220425-unstable.1',
+  title: '1.1.20220425-unstable.1',
   notes: [
-    '追加:コンパクトモードを追加する (#540) by koizuka',
-    '変更:ナビゲーションバーを上部から左に移動し、デザインを変更',
+    '追加: ログインボタンの場所がわかるように初回ヘルプチップを表示 (#552) by koizuka',
+    '変更: 初回インストール時にシーンプリセットの前に空のシーンを作り、そちらをデフォルトとする (#549) by koizuka',
+    '修正: ニコニコのログイン状態判定方法を変更 (#532) by koizuka',
+    '修正: ニコニコミュニティの情報取得方法を更新 (#550) by koizuka',
+    '修正:ウィンドウ下端の青い光を削除 (#555) by yusukess',
+    '修正:（生放送クルーズさんの番組）を読み上げないように (#545) by koizuka',
   ],
 };
