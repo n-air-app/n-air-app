c61871659463ab7ca4a64dd1ba80abbb21026122
Akihiko Koizuka <koizuka@dwango.co.jp>
2022-07-19 13:00:27 +0900

    v1.1.20220719-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,14 +1,10 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20220425-unstable.1',
-  title: '1.1.20220425-unstable.1',
+  version: '1.1.20220719-unstable.1',
+  title: '1.1.20220719-unstable.1',
   notes: [
-    '追加: ログインボタンの場所がわかるように初回ヘルプチップを表示 (#552) by koizuka',
-    '変更: 初回インストール時にシーンプリセットの前に空のシーンを作り、そちらをデフォルトとする (#549) by koizuka',
-    '修正: ニコニコのログイン状態判定方法を変更 (#532) by koizuka',
-    '修正: ニコニコミュニティの情報取得方法を更新 (#550) by koizuka',
-    '修正:ウィンドウ下端の青い光を削除 (#555) by yusukess',
-    '修正:（生放送クルーズさんの番組）を読み上げないように (#545) by koizuka',
-  ],
+    '追加: どのような設定で配信されているのかの統計用データを送信する (#557) by koizuka',
+    '追加: 番組作成成功時にコメント欄下部に通知を表示する (#553) by koizuka',
+  ]
 };
