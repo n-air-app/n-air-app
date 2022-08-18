1dd4b0d6c333d54f2c2403e02a5329fd0edc3bb3
Akihiko Koizuka <koizuka@dwango.co.jp>
2022-08-18 14:01:57 +0900

    v1.1.20220818-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,10 +1,13 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20220719-unstable.1',
-  title: '1.1.20220719-unstable.1',
+  version: '1.1.20220818-unstable.1',
+  title: '1.1.20220818-unstable.1',
   notes: [
-    '追加: どのような設定で配信されているのかの統計用データを送信する (#557) by koizuka',
-    '追加: 番組作成成功時にコメント欄下部に通知を表示する (#553) by koizuka',
+    '修正: エラーメッセージ内のログインボタンの場所を右上から左下に修正 (#571) by koizuka',
+    '修正: 番組終了後の番組作成ボタンからも番組作成通知を表示する (#561) by koizuka',
+    '修正: 起動時のニコニコログインチェックを強化 (#565) by koizuka',
+    '改善:読み上げ辞書に「穣崇」の読み(しげたか)を追加 (#568) by koizuka',
+    '改善:配信開始を押した時の「配信情報が取得出来ない」というダイアログのメッセージ改善 (#570) by koizuka',
   ]
 };
