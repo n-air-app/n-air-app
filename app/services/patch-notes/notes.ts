ee8b835766d360a95c5ea391878163a7d314a963
Akihiko Koizuka <koizuka@dwango.co.jp>
2022-10-03 14:23:00 +0900

    v1.1.20221003-unstable.2

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,11 +1,12 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20221003-unstable.1',
-  title: '1.1.20221003-unstable.1',
+  version: '1.1.20221003-unstable.2',
+  title: '1.1.20221003-unstable.2',
   notes: [
     '更新:ウィンドウキャプチャを改善(obs-studio-nodeをv0.6.36に更新) (#574) by koizuka',
     '変更:ニコ生パネルのメニューの文言: 奨励プログラムに登録する -> この番組で収入を得る (#576) by koizuka',
     '追加:起動ログを送信(統計用) (#575) by koizuka',
+    '修正:ビットレートが0になっていた (#577) by koizuka',
   ]
 };
