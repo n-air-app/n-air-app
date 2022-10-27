3744039e624dcb82562c50900f0733653153a984
Akihiko Koizuka <koizuka@dwango.co.jp>
2022-10-27 14:43:16 +0900

    v1.1.20221027-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,12 +1,10 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20221003-unstable.2',
-  title: '1.1.20221003-unstable.2',
+  version: '1.1.20221027-unstable.1',
+  title: '1.1.20221027-unstable.1',
   notes: [
-    '更新:ウィンドウキャプチャを改善(obs-studio-nodeをv0.6.36に更新) (#574) by koizuka',
-    '変更:ニコ生パネルのメニューの文言: 奨励プログラムに登録する -> この番組で収入を得る (#576) by koizuka',
-    '追加:起動ログを送信(統計用) (#575) by koizuka',
-    '修正:ビットレートが0になっていた (#577) by koizuka',
+    '修正: ダークモードでも起動できるようにする (#580) by koizuka',
+    '修正: 開発時以外はvue devtoolsをinstallしないようにする (#582) by koizuka',
   ]
 };
