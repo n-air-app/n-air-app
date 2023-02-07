8bbf67af767d9b0efda0b2cade9143ea7982c184
Akihiko Koizuka <koizuka@dwango.co.jp>
2023-02-07 12:02:09 +0900

    v1.1.20230207-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,10 +1,11 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20221027-unstable.1',
-  title: '1.1.20221027-unstable.1',
+  version: '1.1.20230207-unstable.1',
+  title: '1.1.20230207-unstable.1',
   notes: [
-    '修正: ダークモードでも起動できるようにする (#580) by koizuka',
-    '修正: 開発時以外はvue devtoolsをinstallしないようにする (#582) by koizuka',
+    '追加: 音声読み上げに N Voiceを追加する (#586) by koizuka',
+    '修正:配信エラー時にセキュリティソフトの案内を追加(日本語) (#584) by Shunsuke-Yanazawa',
+    '修正:配信エラー時にセキュリティソフトの案内を追加(英語) (#585) by Shunsuke-Yanazawa',
   ]
 };
