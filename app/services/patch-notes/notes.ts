4cacca9813159e120a2506d324b976211ad1bcb2
Akihiko Koizuka <koizuka@dwango.co.jp>
2023-02-22 13:20:27 +0900

    v1.1.20230222-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,11 +1,11 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20230207-unstable.1',
-  title: '1.1.20230207-unstable.1',
+  version: '1.1.20230222-unstable.1',
+  title: '1.1.20230222-unstable.1',
   notes: [
-    '追加: 音声読み上げに N Voiceを追加する (#586) by koizuka',
-    '修正:配信エラー時にセキュリティソフトの案内を追加(日本語) (#584) by Shunsuke-Yanazawa',
-    '修正:配信エラー時にセキュリティソフトの案内を追加(英語) (#585) by Shunsuke-Yanazawa',
+    '追加: N Voice 琴詠ニアが youtube, youtuber, vtuber を読める (#593) by koizuka',
+    '追加:コメント読み上げ:振り分け設定のエンジン選択に「読み上げない」を追加 (#590) by koizuka',
+    '改善:コメント読み上げにキューを持ち、5コメント溜まってたら飛ばす (#591) by koizuka',
   ]
 };
