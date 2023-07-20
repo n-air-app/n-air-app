5dc208c2eeb0db9706d9514638a090f4a7befa0f
Akihiko Koizuka <koizuka@dwango.co.jp>
2023-07-20 12:30:26 +0900

    v1.1.20230720-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,11 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20230711-unstable.1',
-  title: '1.1.20230711-unstable.1',
+  version: '1.1.20230720-unstable.1',
+  title: '1.1.20230720-unstable.1',
   notes: [
-    "修正: VLCソース＆アプリ音声キャプチャアイコンを修正 (#673) by yusukess",
-    "修正:ソースのブレンドモードの日本語文言を修正 (#672) by asaday",
-    "修正:ニコ生の番組作成と番組編集ウィンドウから英語のメニューバーを削除 (#669) by koizuka",
+    "修正: インストーラで実行に必要なVisual C++再頒布可能パッケージを自動インストール / 起動時に足りないときもダウンロードを案内する (#677) by koizuka",
   ]
 };
