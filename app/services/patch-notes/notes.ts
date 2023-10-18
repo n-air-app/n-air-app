dac129f91d8ef2c53174b2d85927e4c3f5a5797b
Akihiko Koizuka <koizuka@dwango.co.jp>
2023-10-18 17:04:43 +0900

    v1.1.20231018-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,12 +1,12 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20230828-unstable.1',
-  title: '1.1.20230828-unstable.1',
+  version: '1.1.20231018-unstable.1',
+  title: '1.1.20231018-unstable.1',
   notes: [
-    "追加: 「なふだ機能」 (#670, #684, #690) by koizuka",
-    "修正: アップデート画面（ダウンロード）のデザイン調整 (#680) by yusukess",
-    "修正: アプリ終了処理(shutdownHandler)を改善 (#687) by asaday",
-    "修正: シーンフォルダの無限ループ対策 (#686) by koizuka",
+    "追加: カスタムキャストNDIソース (#706) by koizuka",
+    "修正: OBS初期化タイミングを修正し、初期化エラーダイアログの文言も正しく出るように修正 (#702) by koizuka",
+    "修正: アップデートインストール時の「N Airが終了できない」対策でelectron-builderを22系(22.14.13)に下げてみる (#700) by koizuka",
+    "修正:ストリーミング(配信・録画)エラーダイアログを改善 (#696) by koizuka",
   ]
 };
