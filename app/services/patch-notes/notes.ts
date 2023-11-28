dd9cb6442b4492841d2e575d1e7a44711fd250a5
Akihiko Koizuka <koizuka@dwango.co.jp>
2023-11-28 17:19:05 +0900

    v1.1.20231128-unstable.2

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,12 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20231128-unstable.1',
-  title: '1.1.20231128-unstable.1',
+  version: '1.1.20231128-unstable.2',
+  title: '1.1.20231128-unstable.2',
   notes: [
+    "修正: NG登録してもコメント一覧に反映されなかった (#718) by koizuka",
+    "以下の修正も含みます:",
+    "1.1.20231128-unstable.1",
     "改善: 放送者NG設定画面のリニューアル (#692) by koizuka",
     "追加: 番組の開始/終了のショートカットキー (#710) by koizuka",
     "修正:「最適化して配信」ボタンの連打対策 (#714) by koizuka",
