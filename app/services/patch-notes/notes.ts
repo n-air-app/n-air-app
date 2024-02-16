0a33bc25c06af439222222c23e79f40e436855da
Akihiko Koizuka <koizuka@dwango.co.jp>
2024-02-16 17:32:24 +0900

    v1.1.20240216-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20240116-unstable.1',
-  title: '1.1.20240116-unstable.1',
+  version: '1.1.20240216-unstable.1',
+  title: '1.1.20240216-unstable.1',
   notes: [
-    "更新: 内蔵OBSを少し新しいものに(libOBS 28.2.68) (#708) by asaday",
+    "修正: ニコニコ生放送の最大放送時間を6時間から24時間に変更 (#722) by koizuka",
   ]
 };
