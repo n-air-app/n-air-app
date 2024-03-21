02ead03fc1769df80799d355cb1ca9d4f6c80a22
Akihiko Koizuka <koizuka@dwango.co.jp>
2024-03-21 13:05:19 +0900

    v1.1.20240321-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20240216-unstable.1',
-  title: '1.1.20240216-unstable.1',
+  version: '1.1.20240321-unstable.1',
+  title: '1.1.20240321-unstable.1',
   notes: [
-    "修正: ニコニコ生放送の最大放送時間を6時間から24時間に変更 (#722) by koizuka",
+    "追加: ボイスチェンジャー機能 (#726) by asaday",
   ]
 };
