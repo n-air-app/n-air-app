2dcbaa85b135e54870d607190a13e194ed0450df
Akihiko Koizuka <koizuka@dwango.co.jp>
2024-04-15 13:06:06 +0900

    v1.1.20240415-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,11 +1,11 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20240403-unstable.1',
-  title: '1.1.20240403-unstable.1',
+  version: '1.1.20240415-unstable.1',
+  title: '1.1.20240415-unstable.1',
   notes: [
-    "修正: 放送者NG上限表示を一般会員もプレミアム会員と同じ500に (#735) by koizuka",
-    "修正: 音声デバイスが一つも無いときにボイスチェンジャーソースが削除できなくなっていた (rtvcライブラリを1.0.4に更新) (#729) by asaday",
-    "調査用: ボイスチェンジャーソースが一覧に出ないときに音声ソース一覧とOBSログをSentryに送信する (#737) by koizuka",
+    "修正: 放送者NGを配信用ブロックに文言変更 (#738) by koizuka",
+    "改善: ウィンドウキャプチャソースのプロパティウィンドウを開く時のちらつきを解消 (#742) by matsuyuki-a",
+    "開発: ボイスチェンジャーソースが表示されない環境調査(2) (#739) by koizuka",
   ]
 };
