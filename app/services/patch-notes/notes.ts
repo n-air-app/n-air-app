fa9db1a370df1e5094cc8bbfbadcbaf68aa5fd0a
Akihiko Koizuka <koizuka@dwango.co.jp>
2024-05-27 13:38:49 +0900

    v1.1.20240527-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,8 +1,8 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20240524-unstable.1',
-  title: '1.1.20240524-unstable.1',
+  version: '1.1.20240527-unstable.1',
+  title: '1.1.20240527-unstable.1',
   notes: [
     "追加: モデレーター機能 (#743) by koizuka",
     "追加: ボイスチェンジャーの歌モード (#745, #755, #757) by asaday",
