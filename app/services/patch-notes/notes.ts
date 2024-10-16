9cfa3911683e4a80d721dcf2c5466cee9bb9ee2a
Akihiko Koizuka <koizuka@gmail.com>
2024-10-16 16:21:16 +0900

    v1.1.20241016-unstable.2

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,12 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20241016-unstable.1',
-  title: '1.1.20241016-unstable.1',
+  version: '1.1.20241016-unstable.2',
+  title: '1.1.20241016-unstable.2',
   notes: [
+    "修正: ニコ生配信最適化でOBS29のNVENCのエンコーダープリセット選択肢の変更に対応(llhq -> p3) (#852) by koizuka",
+    "",
+    "以下は 1.1.20241016-unstable.1 の更新内容です:",
     "修正: 「##このコメントは表示されません##」を読み上げない (#846) by koizuka",
     "修正: コメントを再読み込みするとブロックしたはずのコメントのブロックが解除されたように見えていた (#844) by koizuka",
     "修正: シーン切り替え画面などに使われるオーバーレイの外側の背景色が透過していなかった(黒い額縁に見えていた) (#848) by yusukess",
