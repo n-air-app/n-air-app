d5e865ec1a407dd28ad6586bed4e112eacbbbeeb
Akihiko Koizuka <koizuka@gmail.com>
2025-02-13 11:01:09 +0900

    v1.1.20250213-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,13 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20250206-unstable.1',
-  title: '1.1.20250206-unstable.1',
+  version: '1.1.20250213-unstable.1',
+  title: '1.1.20250213-unstable.1',
   notes: [
-    "追加: VOICEVOXを利用したコメント読み上げ (#890) by asaday",
-    "修正: 出力設定(出力モード=詳細)の録画フォーマットの下の音声トラック選択肢が出ていなかった (#885) by koizuka",
-    "改善: n-air-app://settings で設定ウィンドウを開くようにする (#878) by koizuka",
-    "改善: スタジオモードで終了すると、次回起動時にスタジオモードを復元する (#887) by koizuka",
-    "改善: 設定の出力モード:詳細のときの翻訳文言をいくつか補完 (#889) by koizuka",
+    "修正: スタジオモードにしたときにマウスカーソルの判定位置がずれていた (#895) by koizuka",
   ]
 };
