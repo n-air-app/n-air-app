fb23ac16ac1310388764240bf0c42da4bef29da2
Akihiko Koizuka <koizuka@gmail.com>
2025-02-19 12:01:52 +0900

    v1.1.20250219-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,11 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20250213-unstable.1',
-  title: '1.1.20250213-unstable.1',
+  version: '1.1.20250219-unstable.1',
+  title: '1.1.20250219-unstable.1',
   notes: [
-    "修正: スタジオモードにしたときにマウスカーソルの判定位置がずれていた (#895) by koizuka",
+    "追加: ボイスチェンジャーに「東北ずん子」「東北きりたん」など7種類のプリセットキャラクターを追加 (#896) by asaday",
+    "開発: 依存更新 node-fetch to 3.3.2(to avoid punycode warning), prompts to 7.3.2 (#899) by koizuka",
+    "開発: 修正: bin: node-fetchを削除 (#900) by koizuka",
   ]
 };
