e7e8dd9bd0fd61d103652034e1788ca8a65c118f
asaday <asada@nagisaworks.com>
2025-12-23 16:10:59 +0900

    型定義を修正し、propertyComponentForType関数のコンポーネント取得ロジックを改善

--- app/components/obs/inputs/Components.ts
+++ app/components/obs/inputs/Components.ts
@@ -2,15 +2,19 @@ import { Component } from 'vue';
 import * as comps from './index';
 import { TObsType } from './ObsInput';
 
-const inputComponents = comps as any;
+type InputComponent = Component & { obsType: TObsType | TObsType[] };
+
+const inputComponents = comps as Record<string, InputComponent>;
 
 export function propertyComponentForType(type: TObsType): Component {
-  const componentName = Object.keys(inputComponents).find(name => {
-    const componentObsType = inputComponents[name]['obsType'];
-    return Array.isArray(componentObsType)
-      ? componentObsType.includes(type)
-      : componentObsType === type;
+  const component = Object.values(inputComponents).find(comp => {
+    const obsType = comp.obsType;
+    return Array.isArray(obsType) ? obsType.includes(type) : obsType === type;
   });
-  if (!componentName) console.warn('Component not found. Type:', type);
-  return inputComponents[componentName];
+
+  if (!component) {
+    console.warn('Component not found. Type:', type);
+  }
+
+  return component!;
 }
