fbcb51bfd5f32decb77e3ad192b8278b83108b65
asaday <asada@nagisaworks.com>
2026-01-30 14:15:19 +0900

    開発: 未使用のshared/inputsコンポーネントを削除

--- app/components/shared/inputs/ValidatedForm.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <form :data-vv-scope="validationScopeId" @submit.prevent="$emit('submit')">
-    <slot></slot>
-  </form>
-</template>
-
-<script lang="ts" src="./ValidatedForm.vue.ts"></script>
