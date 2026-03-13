30a144cbba0e1d59d25c6734a2b196aa28ecb4d7
Akihiko Koizuka <koizuka@gmail.com>
2026-03-13 10:52:39 +0900

    1.1.20260313-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,13 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20260311-unstable.1',
-  title: '1.1.20260311-unstable.1',
+  version: '1.1.20260313-unstable.1',
+  title: '1.1.20260313-unstable.1',
   notes: [
-    "追加: コメントの読み上げ停止機能 (#1165) by koizuka",
-    "追加: スポイト式カラーピッカーを追加 (#1138) by asaday",
-    "追加: 設定ダイアログに目次(TOC)機能を実装 (#1102) by koizuka",
-    "変更: キャッシュ削除時にシーンコレクションを保持するよう変更 (#1173) by koizuka",
-    "修正: コメントサーバー再接続後にコメントが受信できない問題を修正 (#1167) by koizuka",
+    "修正: 起動時にコメント読み上げ設定がデフォルト値にリセットされる問題を修正 (#1187) by koizuka",
   ],
 };
