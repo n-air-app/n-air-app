789a79d3939c7eda27b22ea3f3a8be02fc2779b5
Akihiko Koizuka <koizuka@gmail.com>
2026-08-25 20:02:38 +0900

    1.1.20260825-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,11 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20260824-unstable.1',
-  title: '1.1.20260824-unstable.1',
+  version: '1.1.20260825-unstable.1',
+  title: '1.1.20260825-unstable.1',
   notes: [
-    '修正: OBS終了時にN Airを再起動できないことがある問題を修正 (#1417) by koizuka',
-    '修正: アプリ終了処理が完了しない状態で設定画面を開くとクラッシュする問題を修正 (#1412) by koizuka',
-    '修正: 配信開始時の通信エラーを別の通信経路で回避 (#1416) by koizuka',
+    '修正: 配信開始時に最適化処理が失敗するとエラーダイアログが表示されず配信できないことがある問題を修正 (#1424) by koizuka',
   ],
 };
